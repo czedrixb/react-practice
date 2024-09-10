75e87c6cd3aaaedc51087f98e5d9a1483ef567cc
czedrixb <czedrixb@gmail.com>
2024-09-10 23:35:18 +0800

    added error handling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,15 +3,23 @@ import Bloglist from "../components/BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setisPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchBlogs = async () => {
       try {
         const response = await fetch("http://localhost:8000/blogs");
+        if (!response.ok) {
+          throw new Error("could not fetch the data");
+        }
         const data = await response.json();
         setBlogs(data);
+        setisPending(false);
+        setError(null);
       } catch (error) {
-        console.log(error);
+        setisPending(false);
+        setError(error.message);
       }
     };
 
@@ -19,12 +27,16 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="flex flex-col space-y-4">
-      {blogs && <Bloglist blogs={blogs} title="All blogs" />}
-      {/* <Bloglist
+    <div>
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      <div className="flex flex-col space-y-4">
+        {blogs && <Bloglist blogs={blogs} title="All blogs" />}
+        {/* <Bloglist
         blogs={blogs.filter((blog) => blog.author === "mario")}
         title="Mario's blogs"
       /> */}
+      </div>
     </div>
   );
 };
